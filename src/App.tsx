0d526e0b2d359914ab88a184668d62c45bf765fb
newcodecareer <815773677@qq.com>
2024-03-10 22:06:15 +1000

    dark mode config

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"} bg="yellow">
-          aside
-        </GridItem>
+        <GridItem area={"aside"}>aside</GridItem>
       </Show>
-      <GridItem area={"main"} bg="blue">
-        main
-      </GridItem>
+      <GridItem area={"main"}>main</GridItem>
     </Grid>
   );
 }
