f431a91e0f7311fcff1947af62bf5c50b34e5b2c
newcodecareer <815773677@qq.com>
2024-04-04 20:59:59 +1000

    display platform icons step 2

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -10,8 +10,11 @@ const CardGrid = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Img src={game.background_image} />
-      <Heading size="xl">{game.name}</Heading>
-      <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
+      <Heading fontSize="2xl">{game.name}</Heading>
+      <PlatformIconList
+        platforms={game.parent_platforms.map((p) => p.platform)}
+      />
+      {/* <PlatformIconList platforms={game.parent_platforms}/> 这样也可以，传过去的参数不一样*/}
     </Card>
   );
 };
