6fb1ef07ec23b2ab2edf59152f352be9e4d56112
newcodecareer <815773677@qq.com>
2024-04-30 22:25:43 +1000

    Clean up the games

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,14 +13,14 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Img src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between" marginBottom={2}>
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           {/* <PlatformIconList platforms={game.parent_platforms}/> 这样也可以，传过去的参数不一样*/}
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
