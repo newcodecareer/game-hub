bec7ca63670d7e97e8546b62d11e2813ca4d884c
newcodecareer <815773677@qq.com>
2026-01-20 15:26:56 +1000

    Build expandable description of game

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,20 +1,36 @@
 import useGameDetail from "../hooks/useGameDetail";
 import { useParams } from "react-router-dom";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
 
   const { data, isLoading, error } = useGameDetail(slug!);
+  const [isOpen, setIsOpen] = useState(false);
   // console.log(gameDetail)
   if (isLoading) return <Spinner />;
   if (error) throw error;
   //if (error || !data) throw
 
+  if (!data?.description_raw) return null;
+  const showDes = data?.description_raw.slice(0, isOpen ? undefined : 300);
+
   return (
     <>
       <Heading>{data?.name}</Heading>
-      <Text>{data?.description_raw}</Text>
+      <Text>
+        {showDes}
+        {data.description_raw.length > 300 && (
+          <Button
+            colorScheme="green"
+            onClick={() => setIsOpen(!isOpen)}
+            paddingY={2}
+            marginX={2}>
+            {isOpen ? "Less" : "More..."}
+          </Button>
+        )}
+      </Text>
     </>
   );
 };
