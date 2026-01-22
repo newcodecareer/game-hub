0672664ee88ffbfe85f03fa94972f9df25a86466
newcodecareer <815773677@qq.com>
2026-01-22 21:08:50 +1000

    Fixing: navbar logo link to homepage, css issue after wrap with link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,19 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-import { Navigate, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding={"10px"}>
       {/* video not right, can use the image address here, but for deploy, ? */}
       {/* TODO change image to pub when deploy */}
-      <NavLink to="/">
-        <Image src={logo} boxSize="60px" width="75px" />
-      </NavLink>
+      <Link to="/">
+        <Box display="block" w="60px" h="60px">
+          <Image src={logo} w="100%" h="100%" />
+        </Box>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
